88c84bf0ad4efb27e38de761e9ea71a465b01fd2
Milanpraz <mln.praz12@gmail.com>
2023-12-14 21:03:16 +0545

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    RESEND_API_KEY: "re_2HQykgnh_FBXUxZ7XXMSpFYrkDszYXX1q",
+    FROM_EMAIL: "mln.praz12@gmail.com",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
